e7bc88fccb0115ec2b8198b8bcb30844ee1c19f6
Warisali099 <126592623+Warisali099@users.noreply.github.com>
2023-08-03 22:17:58 +0530

    Create DETECT_INTEGER.java
    Write a java program to detect whether a number entered by the user is integer or not

---
+++ DETECT_INTEGER.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class DETECT_INTEGER {
+  // Write a java program to detect whether a number entered by the user is integer or not.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number in Integer");
+
+
+//        boolean b1 = sc.hasNextInt();
+//        System.out.println(b1);*/
+//        or
+        System.out.println(sc.hasNextInt());
+    }
+}
